d8b5e0c9f4e5938925293e5fe8910ae1b285ed3c
Jonathan Gonçalves <jonathan_oliveira1997@hotmail.com>
2022-05-10 01:55:40 -0300

    Removendo repetições no código

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,6 +1,7 @@
 class WarehousesController < ApplicationController
+  before_action :set_warehouse, only: [:show, :edit, :update]
+  
   def show
-    @warehouse = Warehouse.find(params[:id])
   end
 
   def new
@@ -8,7 +9,6 @@ class WarehousesController < ApplicationController
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :adress, :zip_code, :area)
     @warehouse = Warehouse.new(warehouse_params)
     if @warehouse.save
     redirect_to root_path, notice: 'Galpão cadastrado com sucesso!'
@@ -19,19 +19,24 @@ class WarehousesController < ApplicationController
   end
 
   def edit
-    @warehouse = Warehouse.find(params[:id])
   end
 
   def update
-    @warehouse = Warehouse.find(params[:id])
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :adress, :zip_code, :area)
     if  @warehouse.update(warehouse_params)
       return redirect_to warehouse_path(@warehouse.id), notice:'Galpão atualizado com sucesso'
     else 
       flash.now[:notice] = 'Não foi possível atualizar o galpão'
       render 'edit'
     end
-    
+  end
+
+  private
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :description, :adress, :zip_code, :area)
   end
 
 end
