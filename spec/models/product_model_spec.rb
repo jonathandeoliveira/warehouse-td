a97d6a967be06ee919111327da6102916a8338e3
Jonathan Gonçalves <jonathan_oliveira1997@hotmail.com>
2022-05-16 11:13:50 -0300

    Cadastro de modelo de produtos ok

--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -1,4 +1,61 @@
 require 'rails_helper'
 
-# RSpec.describe ProductModel, type: :model do
-# end
+RSpec.describe ProductModel, type: :model do
+  describe '#valid?' do
+    context 'cadastro de novo produto' do
+      it 'nome é obrigatório' do
+        # arrange
+        supplier = Supplier.create!(company_name: 'Wayne Enterprises Inc',
+                                    company_register: '70.190.836/0001-81',
+                                    brand_name: 'WayneCorp',
+                                    adress: 'Avenida Almirante Barroso, 81',
+                                    city: 'Rio de Janeiro', state: 'RJ',
+                                    email: 'contato@bwayne.com',
+                                    phone_number: '2198405-5854')
+
+        product = ProductModel.new(name: '', weight: 1000, width: 15,
+                                       height: 4, depth: 6, sku: 'B4TR4NGU3', supplier: supplier)
+        # act
+        result = product.valid?
+        # assert
+        expect(result).to eq false
+      end
+
+      it 'SKU é obrigatório' do
+        # arrange
+        supplier = Supplier.create!(company_name: 'Wayne Enterprises Inc',
+                                    company_register: '70.190.836/0001-81',
+                                    brand_name: 'WayneCorp',
+                                    adress: 'Avenida Almirante Barroso, 81',
+                                    city: 'Rio de Janeiro', state: 'RJ',
+                                    email: 'contato@bwayne.com',
+                                    phone_number: '2198405-5854')
+
+        product = ProductModel.new(name: 'Batrang', weight: 1000, width: 15,
+                                       height: 4, depth: 6, sku: '', supplier: supplier)
+        # act
+        result = product.valid?
+        # assert
+        expect(result).to eq false
+      end
+
+      it 'Peso é obrigatório' do
+        # arrange
+        supplier = Supplier.create!(company_name: 'Wayne Enterprises Inc',
+                                    company_register: '70.190.836/0001-81',
+                                    brand_name: 'WayneCorp',
+                                    adress: 'Avenida Almirante Barroso, 81',
+                                    city: 'Rio de Janeiro', state: 'RJ',
+                                    email: 'contato@bwayne.com',
+                                    phone_number: '2198405-5854')
+
+        product = ProductModel.new(name: 'Batrang', weight: '', width: 15,
+                                       height: 4, depth: 6, sku: 'B4TR4NGU3', supplier: supplier)
+        # act
+        result = product.valid?
+        # assert
+        expect(result).to eq false
+      end
+    end
+  end
+end
---
+++ spec/system/product_models/user_register_product_model_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um modelo de produto' do
+  it 'com sucesso' do
+    #arrange
+    supplier1= Supplier.create!(company_name:'Stark Technology Inc', 
+                        company_register:'00.178.762/0001-82',
+                        brand_name:'Stark Industries', adress:'Rua Vilela, 663',
+                        city:'Tatuapé', state:'SP', 
+                        email:'contato@tstark.com', phone_number:'6799672-3406')
+
+    supplier2= Supplier.create!(company_name:'Wayne Enterprises Inc', 
+                          company_register:'70.190.836/0001-81',
+                          brand_name:'WayneCorp',
+                          adress:'Avenida Almirante Barroso, 81',
+                          city:'Rio de Janeiro', state:'RJ',
+                          email:'contato@bwayne.com', 
+                          phone_number:'2198405-5854')
+    #act
+    visit root_path
+    within('nav') do
+      click_on 'Lista de Produtos'
+    end
+    click_on 'Cadastrar Produto'
+    fill_in 'Nome', with: 'Reator Ark'
+    fill_in 'Peso', with: 300
+    fill_in 'SKU', with: '4RK-R34CT0R'
+    fill_in 'Altura', with: 20
+    fill_in 'Largura', with: 30
+    fill_in 'Profundidade', with: 25
+    select 'Stark Industries', from: 'Fornecedor'
+    click_on 'Cadastrar'
+    #assert
+    expect(page).to have_content 'Produto cadastrado com sucesso'
+    expect(page).to have_content 'Reator Ark'
+    expect(page).to have_content 'SKU: 4RK-R34CT0R'
+    expect(page).to have_content 'Fornecedor: Stark Industries'
+    expect(page).to have_content 'Dimensões: 20cm x 30cm x 25cm'
+    expect(page).to have_content 'Peso: 300g'
+  end
+
+  it 'deve preencher todos os campos' do
+    #arrange
+    supplier= Supplier.create!(company_name:'Stark Technology Inc', 
+                        company_register:'00.178.762/0001-82',
+                        brand_name:'Stark Industries', adress:'Rua Vilela, 663',
+                        city:'Tatuapé', state:'SP', 
+                        email:'contato@tstark.com', phone_number:'6799672-3406')
+    #act
+    visit root_path
+    within('nav') do
+      click_on 'Lista de Produtos'
+    end
+    click_on 'Cadastrar Produto'
+    fill_in 'Nome', with: ''
+    fill_in 'Peso', with: ''
+    fill_in 'SKU', with: ''
+    fill_in 'Altura', with: 20
+    fill_in 'Largura', with: 30
+    fill_in 'Profundidade', with: 25
+    select 'Stark Industries', from: 'Fornecedor'
+    click_on 'Cadastrar'
+    #assert
+    expect(page).to have_content 'Não foi possível cadastrar o produto'
+  end
+
+
+end
